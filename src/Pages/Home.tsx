0d63ed92fa967aee0e80d850c493078fef8d799a
Arpad Varga <arpad.varga90@gmail.com>
2024-06-17 15:51:48 +0200

    initialize project
    vite,
    react,
    tanstack/react-query,
    react-router-dom,
    tailwindcss,
    daisyUI,
    firebase

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,10 @@
+//import "./Home.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello Kéktúra Rajongó</h1>
+    </>
+  );
+}
+
---
+++ src/main.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Pages/Home.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { UserProvider } from "./UserProvider";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+/*  {
+    path: "login",
+    element: (
+      <GuestOnly>
+        <LoginPage />
+      </GuestOnly>
+    ),
+  },
+  {
+    path: "movies",
+    element: (
+      <AuthOnly>
+        <MoviesPage />
+      </AuthOnly>
+    ),
+  },
+*/
+]);
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <UserProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </UserProvider>
+  </React.StrictMode>
+);
