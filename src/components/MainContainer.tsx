01b5252d3eb946768afc5c6c127d9fd29466bfcd
Arpad Varga <arpad.varga90@gmail.com>
2024-06-19 18:29:27 +0200

    add table

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -5,6 +5,26 @@ export default function MainContainer() {
         src="./src/assets/kekkor.jpg"
         alt="image-about-kekor"
       />
+      {/* Stamp list */}
+      <div className="h-[42%] w-[90%] overflow-y-auto">
+        <table className="table table-md table-pin-rows">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Bélyegzőhely ID</th>
+              <th>Táv(km)</th>
+              <th>Menetidő(óó:pp)</th>
+              <th>Szint +/- (m)</th>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      {/* Buttons */}
     </div>
   );
 }
