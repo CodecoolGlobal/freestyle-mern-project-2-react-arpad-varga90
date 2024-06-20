84339192f8fb9d3a9805f4cf771cafb6142aff41
spcsabi <csabaspengler@gmail.com>
2024-06-20 11:22:41 +0200

    tailwind

--- src/Pages/MenuBar/MenuBarPage.tsx
+++ src/Pages/MenuBar/MenuBarPage.tsx
@@ -17,7 +17,7 @@ export default function MenuBarPage() {
   return (
     <>
       <GuestOnly>
-        <div className="bg-gray-800 text-white w-1/4 h-full flex flex-col items-center py-4">
+        <div className="bg-gray-800 text-white w-[20%] h-full flex flex-col items-center py-4 fixed top-0 left-0">
           <button
             disabled={signInGoogleLoading}
             className="btn btn-primary mb-4"
